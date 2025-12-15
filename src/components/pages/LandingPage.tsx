39430aeaf2a7c2c2b4402c95cbdd5ab66ae6adaf
Sameer <sameer@trufe.com>
2025-12-15 13:52:15 +0530

    fix

--- src/components/pages/LandingPage.tsx
+++ src/components/pages/LandingPage.tsx
@@ -1144,7 +1144,7 @@ const handleAdvancedSearch = async () => {
           
           {/* Active filters display */}
           {renderActiveFilters()}
-          
+        
           <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-6">
             {searchResults.map((expert) => (
               <ExpertCard 
